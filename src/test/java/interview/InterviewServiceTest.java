1a268c50333b4f1b0ed1d3a42dd92378fd44c415
Zach Lewis <zach.lewis@kroger.com>
2024-05-16 13:57:53 -0400

    Add test

--- src/test/java/interview/InterviewServiceTest.java
+++ src/test/java/interview/InterviewServiceTest.java
@@ -1,8 +1,8 @@
 package interview;
 
-import org.junit.jupiter.api.Test;
-
 public class InterviewServiceTest
 {
+    // TODO: Add instance of InterviewService
 
+    // TODO: Create a Test for InterviewService
 }
---
+++ src/test/java/interview/complete/CompleteInterviewServiceTest.java
@@ -0,0 +1,23 @@
+package interview.complete;
+
+import io.sds.demo.DemoApplication;
+import io.sds.demo.interview.InterviewService;
+import io.sds.demo.interview.complete.CompleteInterviewService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = {DemoApplication.class})
+public class CompleteInterviewServiceTest
+{
+    @Autowired
+    private CompleteInterviewService service;
+
+    @Test
+    void fetchCandidateNames_passed() {
+        var result = service.fetchCandidateNames(true);
+
+        Assertions.assertEquals(result.size(), 4);
+    }
+}
