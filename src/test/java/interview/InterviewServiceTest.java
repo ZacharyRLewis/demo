c67339140ae0ba6a3f04fb1b966397d1a387f6a1
Zach Lewis <zach.lewis@kroger.com>
2024-07-01 15:11:13 -0400

    Update

--- src/test/java/interview/InterviewServiceTest.java
+++ src/test/java/interview/InterviewServiceTest.java
@@ -1,8 +1,15 @@
 package interview;
 
+import io.sds.demo.DemoApplication;
+import io.sds.demo.interview.InterviewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = {DemoApplication.class})
 public class InterviewServiceTest
 {
-    // TODO: Add instance of InterviewService
+    @Autowired
+    private InterviewService interviewService;
 
     // TODO: Create a Test for InterviewService
 }
